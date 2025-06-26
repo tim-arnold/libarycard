0c115509a877bf10cc0de9b7a4ce690d9654d699
Tim Arnold <tim@weareoutright.com>
2025-06-26 09:04:53 -0400

    Fix contact form error (not using NEXT_PUBLIC_API_URL env var)

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 const API_BASE_URL = process.env.NODE_ENV === 'production' 
-  ? 'https://api.librarycard.tim52.io'
+  ? process.env.NEXT_PUBLIC_API_URL
   : 'http://localhost:8787'
 
 export async function POST(request: NextRequest) {
